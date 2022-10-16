a676fca8edb52913ec8d9a1e8a2eb4e5076d1437
vitori4th <vitorithdasilva@gmail.com>
2022-10-16 18:07:58 -0300

    :sparkles: feat: create another sum functionality

--- src/main.c
+++ src/main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
-   printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
- return 0;
+
+void somatoria(int x, int y){
+   int i;
+   long int soma = 0;
+   for(i = x; i <= y; i = i + 1){
+       soma = soma + i;
+   }
+printf("\nA somatória é %ld\n",soma);
+}
+
+long int somatoriaRecursiva(int x,int y){
+ if(x<=y){
+   long int soma = x;
+       long int resultado = somatoriaRecursiva(x+1, y);
+       printf("%ld - ",resultado);
+   return soma + resultado;
+ }
+ else{
+   return 0;
+ }
 }
 
 int main(void) {
-  
+
    printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
    printf("\n\nDigite o número x: ");
    scanf("%d",&x);
    printf("\n\nDigite o número y: ");
    scanf("%d",&y);
 
-   soma(x,y);
+   printf("\nA somatória é: %ld",somatoriaRecursiva(x,y));
 
  return 0;
 }
