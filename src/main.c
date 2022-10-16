65704c5c8f1e584160176732684a2b87ea9371ad
vitori4th <vitorithdasilva@gmail.com>
2022-10-16 18:06:26 -0300

    :sparkles: feat: create sum functionality

--- src/main.c
+++ src/main.c
@@ -5,3 +5,16 @@ int main(void) {
    printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
  return 0;
 }
+
+int main(void) {
+  
+   printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
+   printf("\n\nDigite o número x: ");
+   scanf("%d",&x);
+   printf("\n\nDigite o número y: ");
+   scanf("%d",&y);
+
+   soma(x,y);
+
+ return 0;
+}
